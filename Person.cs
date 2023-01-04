53f70c292ef9c73c8d5157f3d46856d4710cd3ae
alexfandomers <71307225+alexfandomers@users.noreply.github.com>
2023-01-04 11:32:35 -0300

    IntermediaryD

--- Person.cs
+++ Person.cs
@@ -5,20 +5,23 @@ using System.Threading.Tasks;
 
 namespace IntermediaryProperties
 {
+    
     public class Person
     {
-        //propertie name
-        public string name{ get; set; }
-        //propety datetime
-        public DateTime Birthdate {get; private set; }//private set can read only - can not changed
-
-        //constructor that makes birthdate acessible
+        //(3)constructor to access private set
         public Person(DateTime birthdate)
         {
             Birthdate = birthdate;
         }
 
-        //property declared below
+        //(1)auto implemented property without declare a field
+       //with this code c# internaly create a private field and inplement a get and set access
+       public DateTime Birthdate {get; private set;}//need a constructor to access private set
+
+       //(2)age property(i want this field to be based on the birthdate)
+       //if a declare it like this: public int Age{get; set;} it will not be based on birthdate
+       //instead we gonna do is create a get access and do some logic like below
+       //this property calculate age based on the value of other property
         public int Age
         {
             get
--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-
+ //a way to access a private atribute
+ //A property is a class menber that encapsulates a getter/setter for accessing a field.
+ //We nedd a property to create a getter/setter with less code
 namespace IntermediaryProperties
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //a property is a kind of a class menber that is used for providing access to fields of a class
-            //as a best practice, we must declare fields as private and create public properties to provide access to them.
-
-            var person = new Person(new DateTime(1982, 1, 1));
-            //birthdate was set as private(neeed a constructor to fix it)
-            //person.Birthdate = new DateTime(1982, 1, 1); (if birthdate wasn't private)
-            Console.WriteLine(person.Age);
+           var person = new Person(new DateTime(1982, 1, 1));
+           //person.Birthdate = new DateTime(1982, 1, 1); //now the constructor has parameters
+           System.Console.WriteLine(person.Age);
         }
     }
 }
